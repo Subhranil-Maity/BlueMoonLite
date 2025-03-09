d8e584c5d3a159a0c4e671435d12528b8fdbfe41
Subhranil Maity <fightingdj1@gmail.com>
2025-03-09 18:11:45 +0530

    Fixed Host Name and Os Name mistake

--- server/src/main_server/basic_info.rs
+++ server/src/main_server/basic_info.rs
@@ -5,22 +5,24 @@ use sysinfo::System;
 pub struct BasicInfo {
     pub host_name: String,
     pub host_version: String,
-
+    pub host_os_name: String,
     pub server_version: String,
 }
 
 impl BasicInfo {
-    pub fn new(host_name: String, host_version: String, server_version: String) -> Self {
+    pub fn new(host_name: String, host_version: String,host_os_name: String, server_version: String) -> Self {
         Self {
             host_name,
             host_version,
+            host_os_name,
             server_version
         }
     }
     
     pub fn get_basic_info() -> BasicInfo {
         BasicInfo {
-            host_name: System::name().unwrap_or("Unknown".to_string()),
+            host_name: System::host_name().unwrap_or("Unknown".to_string()),
+            host_os_name: System::name().unwrap_or("Unknown".to_string()),
             host_version: System::kernel_version().unwrap_or("Unknown".to_string()),
             server_version: env!("CARGO_PKG_VERSION").to_string()
         }
